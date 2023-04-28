97519b821d12a21754f9c946f248e28140954971
ashmitha <ashmitha@LAPTOP-2892KGSG>
2023-04-28 08:19:21 +0530

    DemoPractice Commit and push

---
+++ src/test/java/DemoPractice/PracticeProgram1.java
@@ -0,0 +1,11 @@
+package DemoPractice;
+import org.testng.annotations.Test;
+public class PracticeProgram1 
+{
+	@Test
+	public void createProgram()
+	{
+		System.out.println("Practice program");
+	}
+
+}
