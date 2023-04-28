d5a16a72e6e388ba0209d81bfac48c0378a6540d
ashmitha <ashmitha@LAPTOP-2892KGSG>
2023-04-28 08:31:55 +0530

    Commit And Push

---
+++ src/test/java/DemoPractice/PracticeForCommitAndPush.java
@@ -0,0 +1,11 @@
+package DemoPractice;
+import org.testng.annotations.Test;
+public class PracticeForCommitAndPush 
+{
+	@Test
+	public void commitPracticeTest()
+	{
+		System.out.println("Commit and push Practice");
+	}
+
+}
