c5974dd92714dd497a6229c3571b61847d959d8f
ashmitha <ashmitha@LAPTOP-2892KGSG>
2023-05-01 07:27:00 +0530

    Sample program committed

---
+++ src/test/java/DemoPractice/Sample.java
@@ -0,0 +1,13 @@
+package DemoPractice;
+
+import org.testng.annotations.Test;
+
+public class Sample 
+{
+	@Test
+	public void createSamplePrgrom()
+
+	{
+		System.out.println("sample program by framework dev");
+	}
+}
